c6b8277c467cc5be1152a4f9601a5573e5d3fbe4
Alan Walsh <alwalsh@iu.edu>
2023-08-17 13:57:38 -0400

    Script fixes

--- Jetstream/Packer/README.md
+++ Jetstream/Packer/README.md
@@ -4,7 +4,10 @@
 > This build process requires [Packer](https://www.packer.io/) 1.9.2 or later.
 
 1. If you have not already done so, log in to [Exosphere](https://jetstream2.exosphere.app/exosphere/). Among other things, this will provide a network that you can user. Documentation: https://docs.jetstream-cloud.org/ui/exo/exo/.
-2. Create a floating IP address in Jetstream.
+2. Create a floating IP address in Jetstream using the public network
+```shell
+openstack floating ip create public
+```
 3. Initialize Packer
 ```shell
 packer init .
--- Jetstream/Packer/scripts/gen3dev.sh
+++ Jetstream/Packer/scripts/gen3dev.sh
@@ -16,8 +16,9 @@ chmod +x get_helm.sh
 bash ./get_helm.sh
 
 # Caddy
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+sudo apt-get update
+sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy
+sudo apt-get update
+sudo apt-get install -y caddy
