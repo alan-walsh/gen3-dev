207f7bdd4afae95bcccedf6eca684c0e7dd9060e
Alan Walsh <alwalsh@iu.edu>
2024-11-20 16:10:18 -0500

    Bump k9s

--- Jetstream/Packer/scripts/gen3dev.sh
+++ Jetstream/Packer/scripts/gen3dev.sh
@@ -35,6 +35,6 @@ sudo apt-get update
 sudo apt-get install -y caddy
 
 #k9s
-wget https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Linux_amd64.tar.gz
+wget https://github.com/derailed/k9s/releases/download/v0.32.7/k9s_Linux_amd64.tar.gz
 tar -xzvf k9s_Linux_amd64.tar.gz
 sudo cp k9s /usr/local/bin/
